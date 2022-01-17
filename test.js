6b9f1d00fb5f002f257e70c0381025218f9e0c2f
shubhamab1212 <78594998+shubhamab1212@users.noreply.github.com>
2022-01-17 12:26:36 +0530

    test
    test file added

---
+++ test.js
@@ -0,0 +1,27 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const calculator = require('./add.js');
+const prompt = require("prompt-sync")({ sigint: true});
+const a = prompt("enter first number : ");
+const b = prompt("enter second number : ");
+
+describe('Condition', () => {
+	describe('Addition', () => {
+		it('Additon of two numbers', () => {
+			let result=calculator.add(a, b)
+			expect(calculator.add(a, b)).to.equal(result)
+			console.log(result);
+		})
+
+
+    describe('Subtraction', () => {
+        it('substraction of two numbers ', () => {
+			let result=calculator.subtract(a, b)
+            expect(calculator.subtract(a, b)).to.equal(result)
+			console.log(result);
+        })
+})
+	})
+
+})
